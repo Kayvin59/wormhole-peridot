ec0aedfe54c0ee14752fe8f306e88e36274b782a
Kayvin59 <kayvin4@hotmail.com>
2024-12-22 11:26:00 +0100

    ci: update eslint config

--- frontend/components/structure/header/Header.tsx
+++ frontend/components/structure/header/Header.tsx
@@ -6,7 +6,6 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo_container}></div>
-
       <nav className={styles.nav_desktop}></nav>
     </header>
   );
